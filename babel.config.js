573f54b848c1727354c07180d5e8f5f38cc9be62
Llorens19 <llorenssorianodiego@gmail.com>
2025-06-01 19:54:25 +0100

    First commit

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: ['nativewind/babel']
+  };
+};
---
+++ src/Hola.tsx
@@ -0,0 +1,4 @@
+import { JSX } from 'react';
+import { Text } from 'react-native';
+
+export const Hola = () : JSX.Element => <Text>Holaa</Text>;
